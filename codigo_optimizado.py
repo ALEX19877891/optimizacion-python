d9e056bf2ee5d576caf0fd80d4c03b0606fd6cd4
Alex <alex1987nks@gmail.com>
2026-05-24 10:47:58 -0500

    Actualizacion codigo optimizado

--- codigo_optimizado.py
+++ codigo_optimizado.py
@@ -6,18 +6,23 @@ inicio = time.time()
 
 
 def es_primo(n):
+
     if n < 2:
         return False
 
     for i in range(2, int(math.sqrt(n)) + 1):
+
         if n % i == 0:
             return False
 
     return True
 
 
+# ARRAY DE NUMPY
 numeros = np.arange(2, 100000)
 
+
+# LIST COMPREHENSION
 numeros_primos = [n for n in numeros if es_primo(n)]
 
 fin = time.time()
